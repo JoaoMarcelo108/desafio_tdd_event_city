855e47166cac5dd729f54ee55b6b8199ceb44070
JoãoMarcelo108 <j.marcelo.medeiros1999@gmail.com>
2026-01-31 11:30:28 -0300

    City findAllByOrderByNameAsc

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAllByOrderByNameAsc(){
+        List<City> list = repository.findAllByOrderByNameAsc();
+        return list.stream().map(CityDTO::new).toList();
+    }
+
+
+}
